1e19eb27771a751bfabe361550109409c4db5dbd
Ren Dela Cruz <ren_delacruz@outlook.com>
2023-11-24 21:47:16 -0500

    build: added InquiererPy

--- src/git_branch_cleaner/cli.py
+++ src/git_branch_cleaner/cli.py
@@ -1,9 +1,44 @@
-import typer
+from InquirerPy import inquirer
+from InquirerPy.utils import get_style
 
 
 def main() -> None:
-    print("Hello from CLI")
+    style = get_style(
+        {
+            "question": "bold",
+            # "answered_question": "#abb2bf",
+            # "instruction": "#abb2bf",
+        },
+        style_override=False,
+    )
+    branches = inquirer.checkbox(
+        message="Select branches to delete:",
+        choices=[
+            "TICKET-100_this-ticket",
+            "TICKET-454_this-ticket",
+            "TICKET-4230_this-ticket",
+            "TICKET-1544_this-ticket",
+            "TICKET-1234_this-ticket",
+            "TICKET-1324_this-ticket",
+        ],
+        style=style,
+        # enabled_symbol="⬢",
+        # enabled_symbol="💣",
+        # disabled_symbol="  ",
+        # disabled_symbol="◯",
+        # qmark="[?]",
+        # amark=" ? ",
+        transformer=lambda result: f"{len(result)} branch{'es' if len(result) != 1 else ''} selected",
+        instruction="(use <space> to select)",
+        # border=True,
+        show_cursor=False,
+        raise_keyboard_interrupt=False,
+        mandatory=False,
+    ).execute()
+
+    if branches:
+        print(branches)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    main()

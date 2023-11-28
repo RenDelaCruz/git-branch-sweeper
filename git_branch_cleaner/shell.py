898f65808e686a07e2310cc3b9d8c4e34eb0556e
Ren Dela Cruz <ren_delacruz@outlook.com>
2023-11-27 19:23:32 -0500

    fix: do not print to stderr

--- git_branch_cleaner/shell.py
+++ git_branch_cleaner/shell.py
@@ -2,5 +2,5 @@ import subprocess
 
 
 def run_command(command: str) -> str:
-    output = subprocess.check_output(command, shell=True)
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
     return output.decode().strip()

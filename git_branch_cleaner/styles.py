b05292c85b014a131cdbfa4f109179eeeb209c4d
Ren Dela Cruz <ren_delacruz@outlook.com>
2023-11-28 09:26:13 -0500

    refactor: use constant for blue color

--- git_branch_cleaner/styles.py
+++ git_branch_cleaner/styles.py
@@ -2,14 +2,15 @@ from typing import Final
 
 from InquirerPy import utils
 
+BLUE: Final[str] = "#48A8B5"
 GREEN: Final[str] = "#1AAE65"
 ORANGE_BOLD_UNDERLINE: Final[str] = "orange bold underline"
 
 DEFAULT_STYLE: Final = utils.get_style(
     {
         "question": "bold",
-        "answer": "#48A8B5",
-        "pointer": "#48A8B5",
+        "answer": BLUE,
+        "pointer": BLUE,
         "questionmark": "orange bold",
         "answermark": "orange bold",
     },

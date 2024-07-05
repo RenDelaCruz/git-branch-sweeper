5281f9c379fdf8d456099e96f790e361259d8dbb
Ren Dela Cruz <ren_delacruz@outlook.com>
2024-07-05 15:49:02 -0400

    feat: add default branch and merged branch methods

--- git_branch_sweeper/git.py
+++ git_branch_sweeper/git.py
@@ -33,9 +33,21 @@ class Git:
         output = process.run(["git", "rev-parse", "--is-inside-work-tree"])
         return output == "true"
 
-    def get_branches(self) -> Sequence[str]:
-        output = process.run(["git", "branch", "--format", "%(refname:short)%(HEAD)"])
+    def get_branches(self, *, merged: bool | None = None) -> Sequence[str]:
+        branch_command = ["git", "branch", "--format", "%(refname:short)%(HEAD)"]
+        if merged is not None:
+            merged_flag = "--merged" if merged else "--no-merged"
+            branch_command.append(merged_flag)
+
+        output = process.run(branch_command)
         return output.replace(" ", "").split("\n") if output else []
+
+    def get_default_branch(self) -> str:
+        remote = process.run(["git", "remote", "show"])
+        output = process.run(
+            ["git", "symbolic-ref", "--short", f"refs/remotes/{remote}/HEAD"]
+        )
+        return output.removeprefix(f"{remote}/")
 
     def delete(self, branches_to_delete: Sequence[str]) -> str:
         return process.run(["git", "branch", "-D", *branches_to_delete])
